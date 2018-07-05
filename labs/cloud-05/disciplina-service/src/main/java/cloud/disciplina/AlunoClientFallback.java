8e1ddb5b7a5b52f7734133a62363df5c9e153b3f
Ramiro Pedrozo <Ramiro.Pedrozo@gmail.com>
2018-07-05 19:25:22 -0300

    .

--- labs/cloud-05/disciplina-service/src/main/java/cloud/disciplina/AlunoClientFallback.java
+++ labs/cloud-05/disciplina-service/src/main/java/cloud/disciplina/AlunoClientFallback.java
@@ -3,7 +3,9 @@ package cloud.disciplina;
 import java.util.Arrays;
 
 import org.springframework.hateoas.Resources;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AlunoClientFallback implements AlunoClient{
 
 	@Override
---
+++ labs/cloud-06/sercurity-server/src/main/java/com/example/security/server/sercurityserver/SercurityServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.security.server.sercurityserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SercurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SercurityServerApplication.class, args);
+	}
+}
